b24cab66eab08ab26a39969e235617b5b8d84bdd
pedroesmerio <p.h.esmerio@gmail.com>
2021-05-03 18:57:36 -0300

    Create Route for list users info

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,7 @@
 const State = require('../models/State');
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Ad = require('../models/Ad');
 
 module.exports = {
   getStates: async (req, res) => {
@@ -6,7 +9,40 @@ module.exports = {
     res.json({ states });
   },
 
-  info: async (req, res) => {},
+  info: async (req, res) => {
+    let token = req.query.token;
+
+    const user = await User.findOne(token);
+    const state = await State.findById(user.state);
+    const ads = await Ad.find({ idUser: user._id.toString() });
+
+    let adList = [];
+
+    for (let i in ads) {
+      const cat = await Category.findById(ads[i].category);
+
+      adList.push({ ...ads[i], category: cat.slug });
+
+      adList.push({
+        id: ads[i]._id,
+        status: ads[i].images,
+        dateCreated: ads[i].dateCreated,
+        title: ads[i].title,
+        price: ads[i].price,
+        priceNegociable: ads[i].priceNegociable,
+        description: ads[i].description,
+        views: ads[i].views,
+        category: cat.slug,
+      });
+    }
+
+    res.json({
+      name: user.name,
+      email: user.email,
+      state: state.name,
+      ads: adList,
+    });
+  },
 
   editAction: async (req, res) => {},
 };
