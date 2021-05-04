297df2dafc156c3e579775e67dd02b2b223ec500
pedroesmerio <p.h.esmerio@gmail.com>
2021-05-04 18:10:03 -0300

    Create Controllers to list ads infos

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -2,8 +2,20 @@ const Ad = require('../models/Ad');
 const Category = require('../models/Category');
 
 module.exports = {
-  getCategories: async (req, res) => {},
+  getCategories: async (req, res) => {
+    const cats = await Category.find();
 
+    let categories = [];
+
+    for (let i in cats) {
+      categories.push({
+        ...cats[i]._doc,
+        img: `${process.env.BASE}/assets/images/${cats[i].slug}.png`,
+      });
+    }
+
+    res.json({ categories });
+  },
   addAction: async (req, res) => {},
 
   getList: async (req, res) => {},
